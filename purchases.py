2103db8a810cb3bc5caecf48c17e8ea950a52e0b
baekka1 <baekka@bc.edu>
2023-09-21 08:35:18 -0400

    first attempt

---
+++ purchases.py
@@ -0,0 +1,36 @@
+#this program asks users for input data and puts the data into two lists using
+#append in a loop. includes a funtion which adds tax and returns a new list with
+#sales tax added. Then, define an empty dictionary and add each customer as a key
+
+#inputs
+num_purchases = int(input("Number of purchases: "))
+sales_tax = float(input("Sales tax: "))
+
+#global variables
+customer_list = []
+cost_list = []
+cost_list_tax = []
+dictionary = {}
+
+#loop with append
+for i in range(num_purchases):
+    temp_customer = input("Customer: ")
+    temp_cost = int(input("Cost: "))
+    customer_list.append(temp_customer)
+    cost_list.append(temp_cost)
+
+#function to add tax
+def add_tax(my_list, tax):
+    for i in range(len(my_list)):
+        tax_cost = my_list[i]*tax
+        cost_list_tax.append(tax_cost)
+
+#adds the tax
+add_tax(cost_list, sales_tax)
+
+#adds to dictionary
+for i in range(len(customer_list)):
+    dictionary[customer_list[i]] = cost_list_tax[i]
+
+#prints dictionary
+print(dictionary)
